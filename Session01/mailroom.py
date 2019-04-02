21c92fb0dae25973d8cc5ed2dc25bf77ad48137f
Mahmood08 <sirin_ruet@yahoo.com>
2019-04-02 08:13:27 -0700

    added the create report

---
+++ Session01/mailroom.py
@@ -0,0 +1,52 @@
+import sys
+
+donors_name = {'william gates':[3, 3000.45],
+               'Jeff Bezos': [2, 3452.00], 
+               'Paul Allen': [2, 9970.65], 
+               'Mark Zuckerberg': [5,5000.75 ], 
+               'David': [3, 2500.17], 
+               'Michael': [4, 1500.00], 
+               'Allen': [1, 2400.35]}
+
+
+
+prompt = "\n".join(("Please select a menu option below:",
+
+                    "1 - Send a Thank You to an individual",
+
+                    "2 - Create a Report",
+
+                    "3 - Send letters to all donors",
+
+                    "4 - Quit",
+
+                    ">>> "))
+
+
+def create_report(): 
+
+    """Create a detailed report of donors and their history of the amounts they have donated"""
+    
+    donor_report = []
+    for name in donors_name: 
+        total_donation = (donors_name[name][1]) 
+ 
+        donation_times = (donors_name[name][0]) 
+        avg_donate = round((total_donation / donation_times), 2) 
+        donor_report.append((name, total_donation, donation_times, avg_donate)) 
+        donor_report.sort(key=lambda x: x[1] , reverse=True) 
+
+    print("\n\n") 
+    print("{:16} {:20} {:25} {:25}\n".format( "Name", "Total Donation", "Number of Gifts", "Average Gifts")) 
+    for i in donor_report: 
+        print("{:16} {:<20} {:<25} {:<25}".format( i[0], i[1], i[2], i[3])) 
+
+    print("\n\n\n")
+
+def donor_list():
+    """ Updated by Comprehensions"""
+    
+    name_list = [name for name in DONORS_NAME]    
+    return '\n'.join(name_list)       
+
+
